3ae8384800a343f1ade5c120036288d4ce18523c
Atishay Jain <atishay1743@gmail.com>
2021-09-08 00:47:02 +0530

    first commit

---
+++ models/campground.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+
+const campgroundSchema = new Schema({
+    title: String,
+    price: String,
+    description: String,
+    location: String,
+
+    
+});
+
+module.exports = mongoose.model('Campground', campgroundSchema);   // this is the model
