a05edc30de245f88d0c6fec69a542cbfb7d9c201
Atishay Jain <atishay1743@gmail.com>
2021-10-21 03:14:15 +0530

    port fixed

--- app.js
+++ app.js
@@ -162,7 +162,8 @@ app.use((err, req,res,next)=>{
   
 })
 
+const port = process.env.PORT || 3000;
 
-app.listen(3000, () => {
-    console.log('Server started on port 3000');
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
 });
